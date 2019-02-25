b6d2300f1e1909c6651c39f9c10a7781ddfdffc6
vlad <vladkalyuzhnyu@gmail.com>
2019-02-25 14:51:10 +0300

    changes

--- app/src/main/java/androidovshchik/httpclass/MainActivity.java
+++ app/src/main/java/androidovshchik/httpclass/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         client = new HttpClient(getApplicationContext(), new HttpClient.Builder()
-            .certificate(R.raw.debug)
+            //.certificate(R.raw.debug)
             .userAgent(null)
             .timeout(30)
             .create());
